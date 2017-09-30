6a0ff9da0b27897f3ababa4758333fbf503e22e6
Daniel James Williams <v-wildan@microsoft.com>
2017-09-30 11:56:35 -0700

    Removing babel in favor of Typescript compiler.

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -11,7 +11,7 @@ module.exports = function (options) {
 
   return {
     entry: {
-      'app': './src/app.jsx',
+      'app': './src/app.tsx',
       'polyfills': './src/polyfills.js',
       'vendor': './src/vendor.js',
     },
@@ -25,25 +25,12 @@ module.exports = function (options) {
           loader: "awesome-typescript-loader"
         },
         {
-          test: /\.jsx?$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader'
-        },
-        {
           enforce: "pre",
           test: /\.js$/,
           loader: "source-map-loader"
         }
       ]
     },
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    // externals: {
-    //   "react": "React",
-    //   "react-dom": "ReactDOM"
-    // },
     plugins: [
       new DefinePlugin({
         'ENV': ENV,
